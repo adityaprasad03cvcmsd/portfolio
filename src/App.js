2507c942027876f71c6b8f0eb95c1c46914c690f
adityaprasad03cvcmsd <91816884+adityaprasad03cvcmsd@users.noreply.github.com>
2022-02-12 10:04:55 +0530

    changes will be made

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { Home } from './components/Home';
 import {About} from "./components/About";
-
+import Project from './components/Project';
+import Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
       <Home/>
       <About/>
+      <Project/>
+      <Contact/>
     </div>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./About.css";
+import "../App.css";
 import { Div } from "../elements/Div";
 import mypic from "../images/mypic.png"
 
+
 export const About=()=>{
+   
+
     return (
         <Div>
             <div className="container">
@@ -13,10 +17,18 @@ export const About=()=>{
                     </div>
                     <div className="col__2">
                         <h1 className="aboutMe">About Me</h1>
-                        <p className="p__color about_detail">
-                            hello, my name is Aditya Prasad.I am a Masters and BTech graduate.I am currently pursuing full - Stack web development program at Masai.I have work experience of 1½ years.Currently, I am looking
-                            for a job in IT sectors which can challenge me broden my horizons.
+                        <p className="p__color about_detail centering">
+                            Hi, my name is Aditya Prasad. My passion is my work that I put in day in and day out.My love for coding can be seen in my projects.
                         </p>
+                        <p className="p__color about_detail">
+                            I am a FULL-STACK web developer with specalization in MERN stack. I have worked on fronend and backend in my projects.
+                        </p>
+                        <p className="p__color about_detail">
+                            I have 1 & 1/2 years of work experience in two aditional domains that is Facilty Management and Hotel Management.
+                        </p>
+                        <div className="aboutButton d__flex align__items__center">
+                            < a href = "https://drive.google.com/file/d/1mHGTVzrQIoQd27DbvBmQBTtZKEtTcm94/view?usp=sharing" target={"_blank"}> < button className = "about_btn" > Download Resume </button></a >
+                        </div>
                     </div>
                 </div>
             </div>
