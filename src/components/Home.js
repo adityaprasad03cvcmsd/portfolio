62018660efde4b2e308e8e379bc24aac5396a112
adityaprasad03cvcmsd <91816884+adityaprasad03cvcmsd@users.noreply.github.com>
2022-02-19 01:17:20 +0530

    update mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import "../App.css";
 import logo from "../images/logo.png";
+import mobilePic from "../images/mobile.png"
 import {SlideInLeft, SlideInRight, RotateCcw } from 'react-animated-components'
 
 export const Home=()=>{
+    const [mobile,setMobile]=useState(false);
+    const navbarForMobile=()=>{
+        return (<div className="navbarformobile">
+            <a href="#Home" onClick={()=>setMobile(!mobile)}><li className="nav__items mx__15">Home</li></a>
+            <a href="#About" onClick={()=>setMobile(!mobile)}><li className="nav__items mx__15">About</li></a>
+            <a href="#Skills" onClick={()=>setMobile(!mobile)}><li className="nav__items mx__15">Skills</li></a>
+            <a href="#Project" onClick={()=>setMobile(!mobile)}><li className="nav__items mx__15">Projects</li></a>
+            <a href="#Contact" onClick={()=>setMobile(!mobile)}><li className="nav__items mx__15">Contact</li></a>
+        </div>)
+    }
     return (
         <div className="home" id="Home">
             <div className="home__bg">
@@ -22,12 +33,14 @@ export const Home=()=>{
                             <a href="#Project"><li className="nav__items mx__15">Projects</li></a>
                             <a href="#Contact"><li className="nav__items mx__15">Contact</li></a>
                         </ul>
-                    </div>
-                </div>
+                    </div>                    
+                    
+                </div>                
                 <div className="container">
                     <div className="home_content">
+                        <img id="contents"src={mobilePic} alt="contents"onClick={()=>{setMobile(!mobile)}}/>
                         <SlideInLeft durationMs={1000}>
-                            <h1 className="home_line1 pz__10">Hi, I am Aditya Prasad</h1>
+                            <h1 className="home_line1 pz__10">Hi, I am Aditya Prasad {mobile?navbarForMobile():null}</h1>
                         </SlideInLeft>
                         <SlideInRight durationMs={1000}>
                         < h3 className = "home_line2 pz__10" > Full-Stack Web Developer </h3>
